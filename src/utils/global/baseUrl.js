67b5d1e6605e188a40dcf68fdc336504f22b8757
Ancient-Ares <502679186@qq.com>
2018-01-04 16:12:42 +0800

    readme

--- src/utils/global/baseUrl.js
+++ src/utils/global/baseUrl.js
@@ -2,13 +2,13 @@
 const nmwUrl = 'https://webapp.snzfnm.com/agriwebapp/app/'
 
 // 测试地址
-// export let baseUrl = 'http://192.168.1.113:8080'
+// export let baseUrl = 'http://192.168.1.116:8080'
 
 // 正式地址
 export let baseUrl = 'http://121.43.198.37:8080'
 
 // webscoket
-// export let wsUrl = 'ws://192.168.1.113:8080/finance/websocket/'
+// export let wsUrl = 'ws://192.168.1.116:8080/finance/websocket/'
 
 // 正式地址
 export let wsUrl = 'ws://121.43.198.37:8080/finance/websocket/'
